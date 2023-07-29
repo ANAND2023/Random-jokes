c1c329222f3910610cd569b187107bc5eea5d321
Anand Kumar Jaiswal <108394963+ANAND2023@users.noreply.github.com>
2023-07-29 23:00:38 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,156 @@
+var jokes = [
+
+  'Pappu went to a doctor to get a solution of loose motions. Doctor: tell me, whats your problem? Pappu: Suffering from unlimited free outgoing with different',
+  'Manager: What is your qualification? Pappu: Im Ph.D. Manager: What do you mean by Ph.D.? Pappu: Passed high school with difficulty',
+  'Once Rajnikanth went to Switzerland and accidentally dropped his wallet in a building. Since then the building is known as ',
+  'The best day for you in the whole year is April 1 because thats the day that suits you best',
+
+  'Interviewer: What is a skeleton? Sardar: Sir, skeleton is a person who started dieting, but forgot to stop it',
+  'A fast beating heart doesnt always mean love. A blushing face is not always a sign that you’re in love. Sometimes hubog lang! Hahaha',
+  'Every new year’s I resolve to lose 20 pounds and I do. The problem is that I gain 30',
+  'A cute Nurse came for interview. Doctor: What salary do you expect? Nurse: Rs 10,000 Doctor was overjoyed and said: My pleasure. Nurse: With pleasure its Rs 25,000',
+  'Wife: whenever we keep the money in the bags our son steals it, I don’t know what to do? Husband: Keep it in his books. I know he will never touch them!',
+  'Advertisements',
+
+  'New way of writing answers in exams. If you don’t know the answer, then put lines like this: |||||||||| and write below:Scratch here for ANSWERS',
+  'One boy on his way to home with his mom after school, Saw a couple kissing on the road, He suddenly shouted and said look mom, They are fighting for CHEWING GUM.',
+  'Height of Shame. At bus stop a girl was standing with her face covered. A man on bike stops and says s have fun today!Girl replies: Papa it', 'Husband and Wife had a Fight. Wife called Mom: He fought with me again, I am coming to you. Mom: No Sun, he must pay for his mistake, I am coming to stay with you!',
+  'Seriously first time in Indian history. Latest funny event occurred Friends, Petrol is cheaper then Onions in India!',
+  'Advertisements',
+  'The world is here at, Sharad University... Where are you? At a better university.',
+  'A boy got rejected and girl got selected in an interview for same reason. Think? they both had the first two buttons of their shirts open in front of the CEO ... Jokes!',
+  'A man lost on no-mans-land Island. One day he decided to build a wood boat to save his life. Suddenly a hot girl came there and the man use the wood for making bed. Moral - A girl can change your goal.',
+  'Sweet Fact: If a Girl has balance in her cell, then she definitely has a boyfriend and if a boy has sufficient balance in his cell, then he surely does not have any girlfriend.',
+  'Heated gold becomes ornaments, beaten copper become wires, compressed rocks become diamonds and mentally tortured men become Best Husbands',
+
+
+  'On a romantic day titu’s GF asks him, Darling on our engagement day will you give me a ring? Titu: from landline or mobile.',
+  'Height of Social Networking: A girls Facebook status: Im online from Toilet! . Her sister commented on status: come out fast, Im getting emergency!Q: Why did titu take his pregnant wife tiya to Pizza Hut? A: Because they advertised ',
+  'Best advice to young boys: If you want to change the nation, do it now. Once you get married, you wont be able to change even the TV channel',
+  'Why are wives  dangerous than the Mafia? The mafia wants either your money or life... The wives want both',
+  'Madam: Who searched I Love You? Titu: China! Madan: How? Titu: Its has no warranty. If works, till forever. If not, then no ever',
+  'Dog was Chasing Titu Titu runs, but Laughing... A Man asked why are you Laughing? Titu replied I have put Vodafone Sim, but the Hutch network is Following...',
+  'A Secretary came angrily out of boss cabin colleague asked: What happened? She replied: He asked me are you free tonight? I said: Yes, and bastard give me 101 pages of work.',
+  'An astronomer was watching the sky from his telescope Santa Singh was observing him, suddenly a star falls, seeing that Santa Singh shouted, Kya nishana lagaya hai! Waah... Waah',
+  'If you think your boss is stupid. Remember, You would not get the job. If he was smarter',
+  'The heights of Bad Luck A boy and cute girl met last time for their break up. Girl’s father and boys mother caught them. Now they are married couple.',
+  'How to reduce weight? First turn your head to the right and then to the left. Repeat this one whenever you have given something to eat!',
+  'Santa got into a bus on 1st April when conductor asked for ticket. He gave Rs 10 and took the ticket and said April fool. I have pass.',
+  'Madam to Student: Last Semester you were roaming with that girl and this semester, you are roaming with other. What you think of yourself? Boy: Syllabus changed mam.',
+  'Relationship status and singer, Before relationship, Honey Singh! When in relationship: Arjit Singh! After breakup: Jagjit Singh',
+  'English Teacher: One cute and young girl is walking on the road. Change this into a Punjabi exclamatory sentence. Sardar student: Oye, Pataka!',
+  'My name is little dancing man but you can call me dark and every day I do a jig from morning until dark. Fine lassies come from far and wide to watch me shake my touch and if they start to crowd me 1 yell  please'
+  ,
+
+  'Pappu went to a doctor to get a solution of loose motions. Doctor: tell me, whats your problem? Pappu: Suffering from unlimited free outgoing with different',
+  'Manager: What is your qualification? Pappu: Im Ph.D. Manager: What do you mean by Ph.D.? Pappu: Passed high school with difficulty',
+  'Once Rajnikanth went to Switzerland and accidentally dropped his wallet in a building. Since then the building is known as ',
+  'The best day for you in the whole year is April 1 because thats the day that suits you best',
+
+  'Interviewer: What is a skeleton? Sardar: Sir, skeleton is a person who started dieting, but forgot to stop it',
+  'A fast beating heart doesnt always mean love. A blushing face is not always a sign that you’re in love. Sometimes hubog lang! Hahaha',
+  'Every new year’s I resolve to lose 20 pounds and I do. The problem is that I gain 30',
+  'A cute Nurse came for interview. Doctor: What salary do you expect? Nurse: Rs 10,000 Doctor was overjoyed and said: My pleasure. Nurse: With pleasure its Rs 25,000',
+  'Wife: whenever we keep the money in the bags our son steals it, I don’t know what to do? Husband: Keep it in his books. I know he will never touch them!',
+  'Advertisements',
+
+  'New way of writing answers in exams. If you don’t know the answer, then put lines like this: |||||||||| and write below:Scratch here for ANSWERS',
+  'One boy on his way to home with his mom after school, Saw a couple kissing on the road, He suddenly shouted and said look mom, They are fighting for CHEWING GUM.',
+  'Height of Shame. At bus stop a girl was standing with her face covered. A man on bike stops and says s have fun today!Girl replies: Papa it', 'Husband and Wife had a Fight. Wife called Mom: He fought with me again, I am coming to you. Mom: No Sun, he must pay for his mistake, I am coming to stay with you!',
+  'Seriously first time in Indian history. Latest funny event occurred Friends, Petrol is cheaper then Onions in India!',
+  'Advertisements',
+  'The world is here at, Sharad University... Where are you? At a better university.',
+  'A boy got rejected and girl got selected in an interview for same reason. Think? they both had the first two buttons of their shirts open in front of the CEO ... Jokes!',
+  'A man lost on no-mans-land Island. One day he decided to build a wood boat to save his life. Suddenly a hot girl came there and the man use the wood for making bed. Moral - A girl can change your goal.',
+  'Sweet Fact: If a Girl has balance in her cell, then she definitely has a boyfriend and if a boy has sufficient balance in his cell, then he surely does not have any girlfriend.',
+  'Heated gold becomes ornaments, beaten copper become wires, compressed rocks become diamonds and mentally tortured men become Best Husbands',
+
+
+  'On a romantic day titu’s GF asks him, Darling on our engagement day will you give me a ring? Titu: from landline or mobile.',
+  'Height of Social Networking: A girls Facebook status: Im online from Toilet! . Her sister commented on status: come out fast, Im getting emergency!Q: Why did titu take his pregnant wife tiya to Pizza Hut? A: Because they advertised ',
+  'Best advice to young boys: If you want to change the nation, do it now. Once you get married, you wont be able to change even the TV channel',
+  'Why are wives  dangerous than the Mafia? The mafia wants either your money or life... The wives want both',
+  'Madam: Who searched I Love You? Titu: China! Madan: How? Titu: Its has no warranty. If works, till forever. If not, then no ever',
+  'Dog was Chasing Titu Titu runs, but Laughing... A Man asked why are you Laughing? Titu replied I have put Vodafone Sim, but the Hutch network is Following...',
+  'A Secretary came angrily out of boss cabin colleague asked: What happened? She replied: He asked me are you free tonight? I said: Yes, and bastard give me 101 pages of work.',
+  'An astronomer was watching the sky from his telescope Santa Singh was observing him, suddenly a star falls, seeing that Santa Singh shouted, Kya nishana lagaya hai! Waah... Waah',
+  'If you think your boss is stupid. Remember, You would not get the job. If he was smarter',
+  'The heights of Bad Luck A boy and cute girl met last time for their break up. Girl’s father and boys mother caught them. Now they are married couple.',
+  'How to reduce weight? First turn your head to the right and then to the left. Repeat this one whenever you have given something to eat!',
+  'Santa got into a bus on 1st April when conductor asked for ticket. He gave Rs 10 and took the ticket and said April fool. I have pass.',
+  'Madam to Student: Last Semester you were roaming with that girl and this semester, you are roaming with other. What you think of yourself? Boy: Syllabus changed mam.',
+  'Relationship status and singer, Before relationship, Honey Singh! When in relationship: Arjit Singh! After breakup: Jagjit Singh',
+  'English Teacher: One cute and young girl is walking on the road. Change this into a Punjabi exclamatory sentence. Sardar student: Oye, Pataka!',
+  'My name is little dancing man but you can call me dark and every day I do a jig from morning until dark. Fine lassies come from far and wide to watch me shake my touch and if they start to crowd me 1 yell  please'
+  ,
+
+  'Pappu went to a doctor to get a solution of loose motions. Doctor: tell me, whats your problem? Pappu: Suffering from unlimited free outgoing with different',
+  'Manager: What is your qualification? Pappu: Im Ph.D. Manager: What do you mean by Ph.D.? Pappu: Passed high school with difficulty',
+  'Once Rajnikanth went to Switzerland and accidentally dropped his wallet in a building. Since then the building is known as ',
+  'The best day for you in the whole year is April 1 because thats the day that suits you best',
+
+  'Interviewer: What is a skeleton? Sardar: Sir, skeleton is a person who started dieting, but forgot to stop it',
+  'A fast beating heart doesnt always mean love. A blushing face is not always a sign that you’re in love. Sometimes hubog lang! Hahaha',
+  'Every new year’s I resolve to lose 20 pounds and I do. The problem is that I gain 30',
+  'A cute Nurse came for interview. Doctor: What salary do you expect? Nurse: Rs 10,000 Doctor was overjoyed and said: My pleasure. Nurse: With pleasure its Rs 25,000',
+  'Wife: whenever we keep the money in the bags our son steals it, I don’t know what to do? Husband: Keep it in his books. I know he will never touch them!',
+  'Advertisements',
+
+  'New way of writing answers in exams. If you don’t know the answer, then put lines like this: |||||||||| and write below:Scratch here for ANSWERS',
+  'One boy on his way to home with his mom after school, Saw a couple kissing on the road, He suddenly shouted and said look mom, They are fighting for CHEWING GUM.',
+  'Height of Shame. At bus stop a girl was standing with her face covered. A man on bike stops and says s have fun today!Girl replies: Papa it', 'Husband and Wife had a Fight. Wife called Mom: He fought with me again, I am coming to you. Mom: No Sun, he must pay for his mistake, I am coming to stay with you!',
+  'Seriously first time in Indian history. Latest funny event occurred Friends, Petrol is cheaper then Onions in India!',
+  'Advertisements',
+  'The world is here at, Sharad University... Where are you? At a better university.',
+  'A boy got rejected and girl got selected in an interview for same reason. Think? they both had the first two buttons of their shirts open in front of the CEO ... Jokes!',
+  'A man lost on no-mans-land Island. One day he decided to build a wood boat to save his life. Suddenly a hot girl came there and the man use the wood for making bed. Moral - A girl can change your goal.',
+  'Sweet Fact: If a Girl has balance in her cell, then she definitely has a boyfriend and if a boy has sufficient balance in his cell, then he surely does not have any girlfriend.',
+  'Heated gold becomes ornaments, beaten copper become wires, compressed rocks become diamonds and mentally tortured men become Best Husbands',
+
+
+  'On a romantic day titu’s GF asks him, Darling on our engagement day will you give me a ring? Titu: from landline or mobile.',
+  'Height of Social Networking: A girls Facebook status: Im online from Toilet! . Her sister commented on status: come out fast, Im getting emergency!Q: Why did titu take his pregnant wife tiya to Pizza Hut? A: Because they advertised ',
+  'Best advice to young boys: If you want to change the nation, do it now. Once you get married, you wont be able to change even the TV channel',
+  'Why are wives  dangerous than the Mafia? The mafia wants either your money or life... The wives want both',
+  'Madam: Who searched I Love You? Titu: China! Madan: How? Titu: Its has no warranty. If works, till forever. If not, then no ever',
+  'Dog was Chasing Titu Titu runs, but Laughing... A Man asked why are you Laughing? Titu replied I have put Vodafone Sim, but the Hutch network is Following...',
+  'A Secretary came angrily out of boss cabin colleague asked: What happened? She replied: He asked me are you free tonight? I said: Yes, and bastard give me 101 pages of work.',
+  'An astronomer was watching the sky from his telescope Santa Singh was observing him, suddenly a star falls, seeing that Santa Singh shouted, Kya nishana lagaya hai! Waah... Waah',
+  'If you think your boss is stupid. Remember, You would not get the job. If he was smarter',
+  'The heights of Bad Luck A boy and cute girl met last time for their break up. Girl’s father and boys mother caught them. Now they are married couple.',
+  'How to reduce weight? First turn your head to the right and then to the left. Repeat this one whenever you have given something to eat!',
+  'Santa got into a bus on 1st April when conductor asked for ticket. He gave Rs 10 and took the ticket and said April fool. I have pass.',
+  'Madam to Student: Last Semester you were roaming with that girl and this semester, you are roaming with other. What you think of yourself? Boy: Syllabus changed mam.',
+  'Relationship status and singer, Before relationship, Honey Singh! When in relationship: Arjit Singh! After breakup: Jagjit Singh',
+  'English Teacher: One cute and young girl is walking on the road. Change this into a Punjabi exclamatory sentence. Sardar student: Oye, Pataka!',
+]
+
+// document.write(jokes[num]);
+let btn = document.getElementById("btn")
+let mySound;
+let interval = setInterval(() => {
+  btn.style.boxShadow = " rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px";
+  btn.style.padding = "10px"
+}, 200)
+
+function next() {
+ 
+  // clearInterval(interval)
+  btn.style.background = "red";
+  btn.style.boxShadow = " none";
+  btn.style.padding = "8px"
+
+  let num = Math.floor(Math.random() * 100)
+  let para = document.getElementById("para");
+  para.innerHTML = jokes[num]
+
+}
+  next()
+
+function sound(){
+  var audio = new Audio('mouse-click-153941.mp3');
+  audio.play();
+}
+sound()
+
+
